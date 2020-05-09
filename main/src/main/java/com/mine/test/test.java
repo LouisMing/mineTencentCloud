5f8919c5a07ccacc94a13ce0bac390c0b2c6068f
LouisMing <MingLouis@qq.com>
2020-05-09 22:32:25 +0800

    test001

---
+++ main/src/main/java/com/mine/test/test.java
@@ -0,0 +1,15 @@
+package com.mine.test;
+
+import com.googlecode.aviator.AviatorEvaluator;
+
+/**
+ * @author yaoming
+ * @date 20.5.9
+ * @commet
+ */
+public class test {
+    public static void main(String[] args) {
+
+       System.out.println(AviatorEvaluator.execute("p('ss00')"));
+    }
+}
